56b8f37a8e39a81cc80e0f01545b3d9744aa4003
cclauss <cclauss@me.com>
2019-02-10 12:21:09 +0100

    Update upgrade_to_python3.py

--- upgrade_to_python3.py
+++ upgrade_to_python3.py
@@ -2,6 +2,8 @@
 
 # run_cmd.py
 
+
+
 import os
 import sys
 from contextlib import contextmanager
@@ -11,6 +13,7 @@ from typing import Iterable, Tuple, Union
 
 from generate_commit_msg import generate_commit_msg
 
+DIR_BASE = "/github/workspace/"
 NEW_BRANCH_NAME = "modernize-Python-2-codes"
 URL_BASE = "https://github.com"
 USERNAME = getuser()  # Does local username == GitHub username?!?
@@ -97,24 +100,13 @@ def git_remote_add_upstream(upstream_url: str) -> str:
     return cmd(f"git remote add upstream {upstream_url}")
 
 
-def futurizer(upstream_url: str) -> None:
-    parts = upstream_url.split("/")
-    if len(parts) == 2:
-        upstream_url = f"{URL_BASE}/{repo}"
-        parts = upstream_url.split("/")
-    assert len(parts) == 5, ("Invalid URL: username/repo or "
-                             "https://github.com/username/repo")
-    repo_user = parts[-2]
-    repo_name = parts[-1]
-    assert repo_user.lower() != USERNAME.lower(), "Can't work on your own repos"
-    fork_repo()  # Currently a noop!!
-    clone_repo(repo_name)
-    with cd(repo_name):
+def futurizer(repo_dir: str) -> int:
+    with cd(repo_dir):
         flake8_results = flake8_tests()
         print(f"flake8_results:\n{flake8_results}")
         if flake8_results:
-            git_remote_add_upstream(upstream_url)
-            checkout_new_branch(NEW_BRANCH_NAME)
+            # git_remote_add_upstream(upstream_url)
+            # checkout_new_branch(NEW_BRANCH_NAME)
             file_paths = files_with_print_issues(flake8_results)
             print(fix_print(file_paths))  # only files that are broken!
             print(fix_safe_fixes())  # all files
@@ -131,9 +123,7 @@ def futurizer(upstream_url: str) -> None:
 
 
 if __name__ == "__main__":
-    args = sys.argv[1:]
-    # repo = args[0] if args else input("URL: ").strip()
-    repo = args[0] if args else "https://github.com/fossasia/pslab-desktop-apps"
+    repo = DIR_BASE
     assert repo, "Man, you gotta give me something to work with here."
     print(f"pwd: {cmd('pwd')}")
     print(f"repo: {repo}")
