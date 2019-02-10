f5aefcc5867a9c5484c1612dbbbbc7b6fcb74bef
cclauss <cclauss@me.com>
2019-02-10 18:15:59 +0100

    print(f"os.environ: {"\n".join(f"     {key}: {os.getenv(value)}" for key in sorted(os.environ))}")

--- upgrade_to_python3.py
+++ upgrade_to_python3.py
@@ -10,7 +10,8 @@ from typing import Iterable, Tuple, Union
 
 from generate_commit_msg import generate_commit_msg
 
-print(f"os.environ: {json.dumps(os.environ, sort_keys=True, indent=2)}")
+print(f"os.environ: {"\n".join(f"     {key}: {os.getenv(value)}"
+                               for key in sorted(os.environ))}")
 
 DIR_BASE = "/github/workspace/"
 NEW_BRANCH_NAME = "modernize-Python-2-codes"
